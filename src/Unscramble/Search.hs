bcf50273c431df221d995a3e7095d5e2c52c3d1b
Devyn Cairns <devyn.cairns@gmail.com>
2013-12-29 22:25:59 -0800

    clean up Search.hs further

--- src/Unscramble/Search.hs
+++ src/Unscramble/Search.hs
@@ -7,28 +7,38 @@ import qualified Data.HashMap.Strict as H
 import Data.List.Stream
 import Data.Maybe
 import Data.Ord
-import Prelude hiding ((++), filter, foldr, head, notElem, null, map, concat, concatMap, length, lines, splitAt, zip, reverse)
+import Prelude hiding (map, filter, head, concatMap)
 import Unscramble.Score
 import Unscramble.Types
 
 tokenize :: String -> [String]
 tokenize = groupBy (\x y -> [x,y] == "qu")
 
-search :: Search -> ScoringSystem -> String -> Maybe (Score,String,[Coordinate])
-search sy@(grid,_) ss word = putWordIn $ walk firstPaths tokens
-  where (firstToken : tokens) = tokenize word
-        putWordIn             = fmap $ \(a,b) -> (a,word,b)
+search :: Search
+       -> ScoringSystem
+       -> String
+       -> Maybe (Score, String, [Coordinate])
+
+search s@(grid,_) system word = putWordIn $ walk firstPaths tokens
+  where putWordIn             = fmap $ \(a, b) -> (a, word, b)
+        (firstToken : tokens) = tokenize word
         lets                  = letters grid
         firstTokenPositions   = H.lookupDefault [] firstToken lets
         firstPaths            = map (: []) firstTokenPositions
 
-        -- new and improved algorithm written by devyn cairns (devyn.me)
+        chooseBestPath :: [[Coordinate]] -> Maybe (Score, [Coordinate])
+
+        chooseBestPath paths =
+          listToMaybe . sortBy (comparing fst) $
+            map (\path -> (score system s path, path)) paths
+
+        -- new and improved algorithm written by Devyn Cairns
+        -- <devyn.cairns@gmail.com>
         -- thanks buddy
-        walk :: [[Coordinate]] -> [String] -> Maybe (Score,[Coordinate])
+        walk :: [[Coordinate]] -> [String] -> Maybe (Score, [Coordinate])
 
         walk [] _     = Nothing
-        walk paths [] = listToMaybe . sortBy (comparing fst)
-                      $ map (\a -> (score ss sy a,a)) paths
+        walk paths [] = chooseBestPath paths
         walk paths (token : remaining) =
           let tokenPositions = H.lookupDefault [] token lets
               branch path =
@@ -36,12 +46,10 @@ search sy@(grid,_) ss word = putWordIn $ walk firstPaths tokens
                   filter (isNeighbor (head path)) (tokenPositions \\ path)
               paths' = concatMap branch paths
           in  walk paths' remaining
-        {-# INLINE walk #-}
 
-isNeighbor :: (Int,Int) -> (Int,Int) -> Bool
+isNeighbor :: Coordinate -> Coordinate -> Bool
 isNeighbor (x,y) (a,b)
   | (x,y) == (a,b) = False
   | otherwise      = let xa = x - a
                          yb = y - b
                      in  (xa >= -1 && xa <= 1) && (yb >= -1 && yb <= 1)
-{-# INLINE isNeighbor #-}
